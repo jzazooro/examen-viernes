553c33cccc356e5aa4dda44d96585cae5ab5c296
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-09-23 16:25:35 +0200

    commit

--- clases/ejercicio1.py
+++ clases/ejercicio1.py
@@ -1,7 +1,3 @@
-listauno=[1, 1, 1]
-listados=[2, 2, 2]
-listatres=[3, 3, 3]
-listacuatro=[4, 4, 4]
 def sumalistas(lista):
     sumadenotas=sum(lista)
     lista.append([sumadenotas])
--- main.py
+++ main.py
@@ -2,7 +2,36 @@ from clases.ejercicio1 import sumalistas
 from clases.ejercicio2 import
 from clases.ejercicio3 import
 from clases.ejercicio4 import
+
+listauno=[1, 1, 1]
+listados=[2, 2, 2]
+listatres=[3, 3, 3]
+listacuatro=[4, 4, 4]
+
 def main():
 
+    print("Ejercicio 1: ")
+    sumalistas(listauno)
+    print("la primera lista es: ", listauno)
+    sumalistas(listados)
+    print("la segunda lista es: ", listados)
+    sumalistas(listatres)
+    print("la tercera lista es: ", listatres)
+    sumalistas(listacuatro)
+    print("la cuarta lista es: ", listacuatro)
+
+    print("Ejercicio 2: ")
+
+    print("Ejercicio 3: ")
+    a=list(range(11))
+    print("la lista resultante al primer enunciado es: ", a)
+    b=list(range(-10, 1))
+    print("la lista resultante al segundo enunciado es: ", b)
+    c=list(range(0, 21, 2))
+    print("la lista resultante al tercer enunciado es: ", c)
+    d=list(range(-19, 0, 2))
+    print("la lista resultante al cuarto enunciado es: ", d)
+    e=list(range(0, 50, 5))
+    print("la lista resultante al quinto enunciado es: ", e)
 if __name__=='__main__':
     main()
