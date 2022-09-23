a8286012b5074b2bf431e26b4290ba73d5249efe
jzazooro <91785177+jzazooro@users.noreply.github.com>
2022-09-23 16:16:25 +0200

    commit 1

---
+++ clases/ejercicio1.py
@@ -0,0 +1,7 @@
+listauno=[1, 1, 1]
+listados=[2, 2, 2]
+listatres=[3, 3, 3]
+listacuatro=[4, 4, 4]
+def sumalistas(lista):
+    sumadenotas=sum(lista)
+    lista.append([sumadenotas])
---
+++ main.py
@@ -0,0 +1,8 @@
+from clases.ejercicio1 import sumalistas
+from clases.ejercicio2 import
+from clases.ejercicio3 import
+from clases.ejercicio4 import
+def main():
+
+if __name__=='__main__':
+    main()
